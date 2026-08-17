ae19ba37599c2aef12e7a566a9fd53da5de5e6e1
Himesh Thakur <thakurhimesh546@gmail.com>
2026-08-17 23:22:00 +0530

    Fix Render server

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
-const express = require("express");
-const path = require("path");
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const PORT = process.env.PORT || 10000;
 
+// Serve React build
 app.use(express.static(path.join(__dirname, "dist")));
 
-app.get("*", (req, res) => {
+// React routes
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
